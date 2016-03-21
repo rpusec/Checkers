a433603acdfbb2779e54bb261be786c68fa2d88c
Roman Pusec <roman.pusec@gmail.com>
2016-03-21 11:33:56 +0100

    Commented out BoardPawnFactory.js and BoardPawn.class.js files.

--- js/canvas/factory/BoardPawnFactory.js
+++ js/canvas/factory/BoardPawnFactory.js
@@ -1,6 +1,12 @@
+/**
+ * Includes functions to create appropriate pawns for the board. 
+ * @author Roman Pusec
+ * @requires BoardPawn.class.js
+ */
 var BoardPawnFactory = {};
 
 (function(){
+
 	var playerPawnOptions = {
 		strokeStyle: 2,
 		strokeColor: Constants.oddColor,
@@ -15,20 +21,52 @@ var BoardPawnFactory = {};
 		radius: 10
 	};
 
+	/**
+	 * Represents the amount of 'sides' for a polygon-shaped pawn. 
+	 * @see  Easeljs documentation for Graphics.drawPolyStar() function. 
+	 * @type {Integer}
+	 */
 	var sides = null;
 
+	/**
+	 * Creates the pawn list for the player. 
+	 * @see Constants.js for the default amount value. 
+	 * @param  {Integer} amount The amount of pawns to generate. 
+	 * @return {Array}        	List of pawns specified for the player.  
+	 */
 	BoardPawnFactory.createPlayerPawnList = function(amount){
 		return createPawnList(amount, playerPawnOptions);
 	}
 
+	/**
+	 * Creates the pawn list for the player's opponent. 
+	 * @see Constants.js for the default amount value. 
+	 * @param  {Integer} amount The amount of pawns to generate. 
+	 * @return {Array}        	List of pawns specified for the player's opponent.  
+	 */
 	BoardPawnFactory.createOpponentPawnList = function(amount){
 		return createPawnList(amount, opponentPawnOptions);
 	}
 
+	/**
+	 * Sets the sides variable back to null. 
+	 * It's executed when the createPlayerPawnList and createOpponentPawnList have already been
+	 * used, since then the previous sides value doesn't have to be kept. 
+	 */
 	BoardPawnFactory.resetSides = function(){
 		sides = null;
 	}
 
+	/**
+	 * Generates a number of pawns. This function is used by 
+	 * both createOpponentPawnList() and createPlayerPawnList() 
+	 * functions. 
+	 * @see Constants.js for the default amount value. 
+	 * @see BoardPawn.class.js for the list of possible options for a pawn display object. 
+	 * @param  {Integer} amount     The amount of pawns to be specified. 
+	 * @param  {Object} pawnOptions Plain object which contains options for each pawn in the list.  
+	 * @return {Array}             	An array of pawns. 
+	 */
 	function createPawnList(amount, pawnOptions){
 		if(typeof amount !== 'number')
 			amount = Constants.PAWN_AMOUNT;
@@ -39,8 +77,9 @@ var BoardPawnFactory = {};
 		{
 			var newSides = calcSides();
 
-			console.log(newSides + " " + sides);
-
+			//to ensure that player's and opponent's
+			//pawns are always different when it comes
+			//to the number of sides from both of them 
 			if(newSides === sides)
 			{
 				if(newSides + 1 > Constants.MAX_POLIGON_POINT_SIZE)
@@ -69,6 +108,11 @@ var BoardPawnFactory = {};
 		return pawnList;
 	}
 
+	/**
+	 * Calculates a new randomized value for polygon-shaped pawn sides. 
+	 * @see Constants.js for minimum or maxumum polygon point size. 
+	 * @return {Integer} A new randomized amount of polygon sides. 
+	 */
 	function calcSides(){
 		var minSides = Constants.MIN_POLIGON_POINT_SIZE;
 		var maxSides = Constants.MAX_POLIGON_POINT_SIZE;
--- js/canvas/model/BoardPawn.class.js
+++ js/canvas/model/BoardPawn.class.js
@@ -1,5 +1,18 @@
 (function(){
 
+	/**
+	 * Represents a polygon-shaped pawn used by both players. 
+	 * @param {Object} options Represents parameters. 
+	 *                         - {Number} strokeStyle => The width of the pawn's border. 
+	 *                         - {String} strokeColor => The color of the border. 
+	 *                         - {String} bgColor => The background color of the pawn. 
+	 *                         - {Number} radius => The radius of the pawn. 
+	 *                         - {Number} pointSize => The pointiness of each polygon 'spike'. 
+	 *                         - {Integer} sides => The number of sides of the pawn. 
+	 * @author Roman Pusec
+	 * @augments {createjs.Shape}
+	 * @see Easeljs createjs.Shape API documentation. 
+	 */
 	function BoardPawn(options){
 		this.Shape_constructor();
 
