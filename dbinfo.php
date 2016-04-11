ab015f542f1f4d256d8271bc9dd4a0f4e06ea8da
student <student@172.22.101.168>
2016-04-11 14:43:52 +0200

    Added initial business classes. STill work.

--- dbinfo.php
+++ dbinfo.php
@@ -5,8 +5,7 @@ define('DB_PASSWORD', '');
 define('DB_NAME', 'rpusec_wad');
 
 /*
-- common prelazi u config
- - hirearhija validatora
  - controlleri osloboditi funkcionalnosti, introduce business layer
  - controller.js refactorirati, separation of concerns
- - Board => make it a collection of pieces i implementiraj svu logiku micanja piecova i referiranja piecova*/
+ - Board => make it a collection of pieces i implementiraj svu logiku micanja piecova i referiranja piecova
+ - validatori u business layeru u svom folderu */
